1a5a2263dea78726372586ba72cd91b0ef1f2d51
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-08-13 01:17:22 +0000

    Added blog page.

---
+++ js/blog.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  //autoplay: true,
+  speed: 400,
+  spaceBetween: 50,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
