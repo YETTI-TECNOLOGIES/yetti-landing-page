ac60ecb81be9eb5e79a0ff4a6226e4897f7a7663
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-08-25 14:54:56 +0000

    Got details of all countries to add to step form.

--- js/register.js
+++ js/register.js
@@ -11,6 +11,13 @@ const regFormContainer = document.querySelector(".signup_container");
 const regForm = document.querySelector(".signup_form");
 
 const stepContainer = document.querySelector(".get-started");
+const gsSLider = document.querySelector(".gs_slider");
+const gsPrev = document.querySelectorAll(".gs_slide--left");
+const gsNext = document.querySelectorAll(".gs_next");
+const gsSkip = document.querySelectorAll(".skip");
+const gsInit = document.querySelector(".gs_init");
+
+const countryList = document.querySelector("#business-location");
 
 /*
 
@@ -24,6 +31,22 @@ function hideRegForm() {
   window.scrollTo(0, 0);
 }
 
+let counter = 0;
+let slidesLength = 4;
+
+function nextSlide() {
+  if (counter === slidesLength) return;
+  counter++;
+  gsSLider.style.transform = `translateX(-${counter * 20}%)`;
+  stepContainer.scrollIntoView();
+}
+
+function prevSlide() {
+  if (counter === 0) return;
+  counter--;
+  gsSLider.style.transform = `translateX(-${counter * 20}%)`;
+}
+
 /*
 
 
@@ -33,4 +56,45 @@ function hideRegForm() {
 regForm.addEventListener("submit", function (e) {
   e.preventDefault();
   hideRegForm();
+});
+
+gsInit.addEventListener("click", function () {
+  nextSlide();
+});
+
+gsNext.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    nextSlide();
+  });
+});
+
+gsSkip.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    nextSlide();
+  });
+});
+
+gsPrev.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    prevSlide();
+  });
+});
+
+/*
+
+
+===================================== API CALLS ===============================================
+
+*/
+let countries = [];
+axios.get("https://restcountries.com/v3.1/all").then(function (response) {
+  const countryInfo = response.data;
+  for (const value of countryInfo.values()) {
+    let country = {
+      name: value.name.common,
+      flag: value.flags.png,
+    };
+    countries.push(country);
+  }
+  console.log(countries);
 });
